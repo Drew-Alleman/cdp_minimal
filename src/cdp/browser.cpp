b4c657854f3d945aac74b7f8cb2659b9d9fe89a2
drew.alleman <drew.allemann@gmail.com>
2026-07-12 15:20:35 -0700

    added MacOS user agent

--- src/cdp/browser.cpp
+++ src/cdp/browser.cpp
@@ -928,6 +928,10 @@ __onMouse(JSON.stringify(data));
                 req.set(http::field::user_agent,
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                     "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+#elif defined(__APPLE__)
+                req.set(http::field::user_agent,
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+                    "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
 #else
                 req.set(http::field::user_agent,
                     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
